cad4816708bfef0c35b7993f384215342aefb57f
liaoyu <627079223@qq.com>
2019-01-01 16:17:39 +0800

    1

---
+++ alien_invasion.py
@@ -0,0 +1,31 @@
+import sys
+
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    #
+    pygame.init()
+    ai_settings = Settings()
+	
+    screen =  pygame.display.set_mode(
+	(ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    
+	
+	
+    #创建一艘飞船
+    ship=Ship(ai_settings,screen)
+    bg_color = (230,230,230)
+    
+	#开始游戏主循环
+    while True:
+
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings,screen,ship)
+
+run_game()
